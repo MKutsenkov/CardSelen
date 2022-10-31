873c2cd111eaf1ed3ec7143e8f302218c7c4c89a
Mikhail Kutsenkov <km36rus@gmail.com>
2022-10-31 15:22:36 +0300

    2 commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 
+[![Build status](https://ci.appveyor.com/api/projects/status/eammgq7w5el4tuu8?svg=true)](https://ci.appveyor.com/project/MKutsenkov/cardselen)
--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -1,10 +1,48 @@
 package ru.netology;
 
-public class CardTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-    ChromeOptions options = new ChromeOptions();
-options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FormTest {
+    private WebDriver driver;
+    private static ChromeOptions options;
+
+    @BeforeAll
+    static void setUpAll() {
+        options = new ChromeOptions();
         options.addArguments("--headless");
+        System.setProperty("webdriver.chrome.driver", "driver/linux/chromedriver");
+    }
+
+    @BeforeEach
+    void setUp() {
         driver = new ChromeDriver(options);
-        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void testPositiveAllInput() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван Иванов");
+        elements.get(1).sendKeys("+79202223344");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
